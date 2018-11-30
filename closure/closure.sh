bfa9e1fee54b4f447ec26ceeccf48dfaefdfe8a7
kawai <kawai@192.168.2.4>
2018-11-30 10:15:25 +0900

    フッター更新

--- closure/closure.sh
+++ closure/closure.sh
@@ -1,6 +1,6 @@
-java -jar ./closure-compiler-v20181125.jar --js ./js/common-25.js --js_output_file ../WebContent/js/common-25.js
+java -jar ./closure-compiler-v20181125.jar --js ./js/common-26.js --js_output_file ../WebContent/js/common-26.js
 #java -jar ./closure-compiler-v20181125.jar --js ./js/commonPc-03.js --js_output_file ../WebContent/js/commonPc-03.js
-#java -jar ./closure-compiler-v20181125.jar --js ./js/upload-15.js --js_output_file ../WebContent/js/upload-15.js
+java -jar ./closure-compiler-v20181125.jar --js ./js/upload-16.js --js_output_file ../WebContent/js/upload-16.js
 
 #for file in $( ls ./js | grep .js$ ); do
 #echo "${file}"
