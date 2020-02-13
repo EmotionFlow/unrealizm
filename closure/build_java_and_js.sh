8b60b7697ec6fc3e36423e930740dc069fc8d3a8
nino-rudel <masahiro_ninomiya@rudel.jp>
2020-02-13 10:06:23 +0900

    一部javaファイルがコンパイルされていなかった不具合修正

--- closure/build_java_and_js.sh
+++ closure/build_java_and_js.sh
@@ -6,10 +6,12 @@ DEPLOY_PATH="../WebContent/WEB-INF/classes"
 CLASSES_WEB_INF="../WebContent/WEB-INF/lib/*"
 CLASSES_TOMCAT="/usr/local/tomcat/lib/*"
 JAVA_FILE_ROOT="../src/jp/pipa/poipiku"
-JAVA_FILE_DIRS=("" "/controller" "/servlet" "/util")
+JAVA_FILE_DIRS=("/controller" "/servlet" "/util")
 
 rm -rf $DEST_PATH
 mkdir $DEST_PATH
+
+javac -sourcepath $SOURCE_PATH -d $DEST_PATH -cp $CLASSES_WEB_INF:$CLASSES_TOMCAT:$DEST_PATH $JAVA_FILE_ROOT/*.java
 
 for dir in $JAVA_FILE_DIRS; do
     javac -sourcepath $SOURCE_PATH -d $DEST_PATH -cp $CLASSES_WEB_INF:$CLASSES_TOMCAT:$DEST_PATH $JAVA_FILE_ROOT$dir/*.java
