a49736a737ab6978055a9b3d7c44e4dfaacbc1c5
nino-rudel <masahiro_ninomiya@rudel.jp>
2020-05-28 15:19:49 +0900

    #130 期限切れ関連実装

--- src/jp/pipa/poipiku/controller/CheckCreditCardC.java
+++ src/jp/pipa/poipiku/controller/CheckCreditCardC.java
@@ -9,6 +9,8 @@ import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
 
 public class CheckCreditCardC {
 	public int getResults(CheckLogin cCheckLogin) {
@@ -25,14 +27,46 @@ public class CheckCreditCardC {
 			dsPostgres = (DataSource)new InitialContext().lookup(Common.DB_POSTGRESQL);
 			cConn = dsPostgres.getConnection();
 
-			//TODO カードの有効期限チェック・レコード更新日時チェック（更新日から四百日経つと使えない）
-			strSql = "SELECT 1 FROM mdk_creditcards WHERE user_id=?";
+			strSql = "SELECT expire, updated_at FROM mdk_creditcards WHERE user_id=?";
 			cState = cConn.prepareStatement(strSql);
 			cState.setInt(1, cCheckLogin.m_nUserId);
 			Log.d("cCheckLogin.m_nUserId", Integer.toString(cCheckLogin.m_nUserId));
 			cResSet = cState.executeQuery();
-			if (cResSet.next()) {
-				nResult = 1;
+			Timestamp updatedAt = null;
+			String strCardExpire = null;
+			if(cResSet.next()){
+				updatedAt = cResSet.getTimestamp("updated_at");
+				strCardExpire = cResSet.getString("expire");
+			}
+			cResSet.close();cResSet=null;
+			cState.close();cState=null;
+
+			if(updatedAt != null) {
+				boolean bExpired = false;
+				if(updatedAt.getTime() + 300 * 24 * 3600 * 1000 < System.currentTimeMillis() ){
+					Log.d("CC_EXPIRE 最終取引から３００日経過している" + cCheckLogin.m_nUserId);
+					bExpired = true;
+				}else{
+					int mm = Integer.parseInt(strCardExpire.split("/")[0]);
+					int yy = Integer.parseInt(strCardExpire.split("/")[1]);
+					LocalDateTime dtCardExpire = LocalDateTime.of(2000+yy, mm, 1, 0, 0, 0);
+					Log.d(dtCardExpire.toString());
+					if(dtCardExpire.plusMonths(-1).compareTo(LocalDateTime.now()) < 0) {
+						Log.d("CC_EXPIRE カードの期限が一ヶ月を切っている " + cCheckLogin.m_nUserId);
+						bExpired = true;
+					}
+				}
+				if(bExpired){
+					strSql = "DELETE FROM mdk_creditcards WHERE user_id=?";
+					cState = cConn.prepareStatement(strSql);
+					cState.setInt(1, cCheckLogin.m_nUserId);
+					cState.executeUpdate();
+					Log.d("CC_EXPIRE カード情報削除 " + cCheckLogin.m_nUserId);
+					cState.close();cState=null;
+					nResult = 0;
+				} else {
+					nResult = 1;
+				}
 			} else {
 				nResult = 0;
 			}
