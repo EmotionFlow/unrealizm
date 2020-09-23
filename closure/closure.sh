9625d5595ef9b53bc073725ce5e7c51ea6323b3f
kauuai <kawai@pipa.jp>
2020-09-23 22:54:37 +0900

    デフォルトでポチ袋をOFFに変更

--- closure/closure.sh
+++ closure/closure.sh
@@ -1,6 +1,6 @@
 java -jar ./closure-compiler-v20181125.jar --js ./js/common-40.js --js_output_file ../WebContent/js/common-40.js
 #java -jar ./closure-compiler-v20181125.jar --js ./js/commonPc-03.js --js_output_file ../WebContent/js/commonPc-03.js
-java -jar ./closure-compiler-v20181125.jar --js ./js/upload-23.js --js_output_file ../WebContent/js/upload-23.js
+java -jar ./closure-compiler-v20181125.jar --js ./js/upload-24.js --js_output_file ../WebContent/js/upload-24.js
 java -jar ./closure-compiler-v20181125.jar --js ./js/update-03.js --js_output_file ../WebContent/js/update-03.js
 
 #for file in $( ls ./js | grep .js$ ); do
