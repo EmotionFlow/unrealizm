6a90f454bf31c38c33ac55ee3c53216867314863
kauuai <kawai@pipa.jp>
2020-11-07 14:46:59 +0900

    フォローワードUserCache対応

--- src/jp/pipa/poipiku/controller/MyHomeTagPcC.java
+++ src/jp/pipa/poipiku/controller/MyHomeTagPcC.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.sql.*;
 
 import jp.pipa.poipiku.*;
+import jp.pipa.poipiku.cache.CacheUsers0000;
 import jp.pipa.poipiku.util.*;
 
 public class MyHomeTagPcC {
@@ -33,150 +34,171 @@ public class MyHomeTagPcC {
 	public int m_nEndId = -1;
 
 	public boolean getResults(CheckLogin cCheckLogin) {
-		return getResults(cCheckLogin, false);
-	}
-
-	public boolean getResults(CheckLogin cCheckLogin, boolean bContentOnly) {
 		boolean bRtn = false;
-		DataSource dsPostgres = null;
-		Connection cConn = null;
-		PreparedStatement cState = null;
-		ResultSet cResSet = null;
+		DataSource dataSource = null;
+		Connection connection = null;
+		PreparedStatement statement = null;
+		ResultSet resultSet = null;
 		String strSql = "";
 		int idx = 1;
 
 		try {
-			dsPostgres = (DataSource)new InitialContext().lookup(Common.DB_POSTGRESQL);
-			cConn = dsPostgres.getConnection();
+			CacheUsers0000 users  = CacheUsers0000.getInstance();
+			dataSource = (DataSource)new InitialContext().lookup(Common.DB_POSTGRESQL);
+			connection = dataSource.getConnection();
 
 			// サーチタグの存在確認
 			boolean bSearchTag = false;
 			strSql = "SELECT tag_txt FROM follow_tags_0000 WHERE user_id=? AND type_id=0 LIMIT 1";
-			cState = cConn.prepareStatement(strSql);
-			cState.setInt(1, cCheckLogin.m_nUserId);
-			cResSet = cState.executeQuery();
-			bSearchTag = cResSet.next();
-			cResSet.close();cResSet=null;
-			cState.close();cState=null;
+			statement = connection.prepareStatement(strSql);
+			statement.setInt(1, cCheckLogin.m_nUserId);
+			resultSet = statement.executeQuery();
+			bSearchTag = resultSet.next();
+			resultSet.close();resultSet=null;
+			statement.close();statement=null;
 
 			// サーチキーワード
-			String strSearchKeyword = SqlUtil.getSearhKeyWord(cConn, cCheckLogin.m_nUserId);
+			strSql = "SELECT tag_txt FROM follow_tags_0000 WHERE user_id=? AND type_id=1 LIMIT 100";
+			statement = connection.prepareStatement(strSql);
+			statement.setInt(1, cCheckLogin.m_nUserId);
+			resultSet = statement.executeQuery();
+			StringBuilder sbKeyWord = new StringBuilder();
+			if(resultSet.next()) {
+				sbKeyWord.append(Util.toString(resultSet.getString(1)).trim());
+				while (resultSet.next()) {
+					sbKeyWord.append(" OR ");
+					sbKeyWord.append(Util.toString(resultSet.getString(1)).trim());
+				}
+			}
+			resultSet.close();resultSet=null;
+			statement.close();statement=null;
+			String strSearchKeyword = sbKeyWord.toString();
+			String strCondSearch = (!strSearchKeyword.isEmpty())?"OR contents_0000.content_id IN(SELECT content_id FROM contents_0000 WHERE description &@~ ?) ":"";
 
 			// サーチタグ&サーチキーワード共に無い場合はそのまま終了
 			if (!bSearchTag && strSearchKeyword.isEmpty()) return true;
 
-			// ミュートキーワード
-			String strMuteKeyword = "";//SqlUtil.getMuteKeyWord(cConn, cCheckLogin.m_nUserId);
-
 			// ブロックユーザクエリ
-			String strCondBlock = SqlUtil.getBlockUserSql(cConn, cCheckLogin.m_nUserId);
+			String strCondBlock = "";
+			strSql = "SELECT block_user_id FROM blocks_0000 WHERE user_id=? LIMIT 1";
+			statement = connection.prepareStatement(strSql);
+			statement.setInt(1, cCheckLogin.m_nUserId);
+			resultSet = statement.executeQuery();
+			if(resultSet.next()) {
+				strCondBlock = "AND contents_0000.user_id NOT IN(SELECT block_user_id FROM blocks_0000 WHERE user_id=?) ";
+			}
+			resultSet.close();resultSet=null;
+			statement.close();statement=null;
+
 			// 非ブロックユーザクエリ
-			String strCondBlocked = SqlUtil.getBlockedUserSql(cConn, cCheckLogin.m_nUserId);
-			// サーチキーワードクエリ
-			String strCondSearch = (!strSearchKeyword.isEmpty())?" OR contents_0000.content_id IN(SELECT content_id FROM contents_0000 WHERE description &@~ ?)":"";
-			// ミュートキーワードクエリ
-			String strCondMute = (!strMuteKeyword.isEmpty())?" AND contents_0000.content_id NOT IN(SELECT content_id FROM contents_0000 WHERE description &@~ ?)":"";
+			String strCondBlocked = "";
+			strSql = "SELECT user_id FROM blocks_0000 WHERE block_user_id=? LIMIT 1";
+			statement = connection.prepareStatement(strSql);
+			statement.setInt(1, cCheckLogin.m_nUserId);
+			resultSet = statement.executeQuery();
+			if(resultSet.next()) {
+				strCondBlocked = "AND contents_0000.user_id NOT IN(SELECT user_id FROM blocks_0000 WHERE block_user_id=?) ";
+			}
+			resultSet.close();resultSet=null;
+			statement.close();statement=null;
+
+			// MUTE KEYWORD
+			String strMuteKeyword = "";
+			String strCondMute = "";
+			if(cCheckLogin.m_bLogin && cCheckLogin.m_nPremiumId>=CUser.PREMIUM_ON) {
+				strMuteKeyword = SqlUtil.getMuteKeyWord(connection, cCheckLogin.m_nUserId);
+				if(!strMuteKeyword.isEmpty()) {
+					strCondMute = "AND content_id NOT IN(SELECT content_id FROM contents_0000 WHERE description &@~ ?) ";
+				}
+			}
+
+			// PC版右ペイン用
+			idx = 1;
+			strSql = "SELECT COUNT(*) FROM contents_0000 WHERE user_id=?";
+			statement = connection.prepareStatement(strSql);
+			statement.setInt(idx++, cCheckLogin.m_nUserId);
+			resultSet = statement.executeQuery();
+			if (resultSet.next()) {
+				m_nContentsNumTotal = resultSet.getInt(1);
+			}
+			resultSet.close();resultSet=null;
+			statement.close();statement=null;
+
+
+			String strSqlFromWhere = "FROM contents_0000 "
+					+ "LEFT JOIN follows_0000 ON contents_0000.user_id=follows_0000.follow_user_id AND follows_0000.user_id=? "
+					+ "WHERE open_id<>2 "
+					+ strCondBlock
+					+ strCondBlocked
+					+ "AND safe_filter<=? "
+					+ "AND (content_id IN (SELECT content_id FROM tags_0000 WHERE tag_txt IN(SELECT tag_txt FROM follow_tags_0000 WHERE user_id=? AND type_id=0) AND tag_type=1) "
+					+ strCondSearch
+					+ ") "
+					+ strCondMute;
 
 			// NEW ARRIVAL
-			if(!bContentOnly) {
-				StringBuilder sb = new StringBuilder();
-				sb.append("SELECT count(*)");
-				sb.append(" FROM contents_0000 INNER JOIN users_0000 ON contents_0000.user_id=users_0000.user_id");
-				sb.append(" LEFT JOIN follows_0000 ON contents_0000.user_id=follows_0000.follow_user_id AND follows_0000.user_id=?");
-				sb.append(" WHERE open_id<>2");
-				sb.append(strCondBlock);
-				sb.append(strCondBlocked);
-				sb.append(" AND safe_filter<=?");
-				sb.append(" AND (content_id IN (SELECT content_id FROM tags_0000 WHERE tag_txt IN(SELECT tag_txt FROM follow_tags_0000 WHERE user_id=? AND type_id=0) AND tag_type=1)");
-				sb.append(strCondSearch);
-				sb.append(")");
-				sb.append(strCondMute);
-				cState = cConn.prepareStatement(sb.toString());
-				idx = 1;
-				cState.setInt(idx++, cCheckLogin.m_nUserId); // follows_0000.user_id=?
-				if(!strCondBlock.isEmpty()) cState.setInt(idx++, cCheckLogin.m_nUserId); // blocks_0000.user_id=?
-				if(!strCondBlocked.isEmpty()) cState.setInt(idx++, cCheckLogin.m_nUserId); // blocks_0000.block_user_id=?
-				cState.setInt(idx++, cCheckLogin.m_nSafeFilter); // safe_filter<=?
-				cState.setInt(idx++, cCheckLogin.m_nUserId); // follow_tags_0000.user_id=?
-				if(!strCondSearch.isEmpty()) cState.setString(idx++, strSearchKeyword);
-				if(!strCondMute.isEmpty()) cState.setString(idx++, strMuteKeyword);
-				cResSet = cState.executeQuery();
-				if (cResSet.next()) {
-					m_nContentsNum = cResSet.getInt(1);
-				}
-				cResSet.close();cResSet=null;
-				cState.close();cState=null;
-
-				// PC版右ペイン用
-				idx = 1;
-				strSql = "SELECT COUNT(*) FROM contents_0000 WHERE user_id=?";
-				cState = cConn.prepareStatement(strSql);
-				cState.setInt(idx++, cCheckLogin.m_nUserId);
-				cResSet = cState.executeQuery();
-				if (cResSet.next()) {
-					m_nContentsNumTotal = cResSet.getInt(1);
-				}
-				cResSet.close();cResSet=null;
-				cState.close();cState=null;
-			}
-
-			StringBuilder sb = new StringBuilder();
-			sb.append("SELECT contents_0000.*, nickname, ng_reaction, users_0000.file_name as user_file_name, follows_0000.follow_user_id");
-			sb.append(" FROM contents_0000 INNER JOIN users_0000 ON contents_0000.user_id=users_0000.user_id");
-			sb.append(" LEFT JOIN follows_0000 ON contents_0000.user_id=follows_0000.follow_user_id AND follows_0000.user_id=?");
-			sb.append(" WHERE open_id<>2");
-			sb.append(strCondBlock);
-			sb.append(strCondBlocked);
-			sb.append(" AND safe_filter<=?");
-			sb.append(" AND (content_id IN (SELECT content_id FROM tags_0000 WHERE tag_txt IN(SELECT tag_txt FROM follow_tags_0000 WHERE user_id=? AND type_id=0) AND tag_type=1)");
-			sb.append(strCondSearch);
-			sb.append(")");
-			sb.append(strCondMute);
-			sb.append(" ORDER BY content_id DESC OFFSET ? LIMIT ?");
-			cState = cConn.prepareStatement(sb.toString());
+			strSql = "SELECT count(contents_0000.*) " + strSqlFromWhere;
+			statement = connection.prepareStatement(strSql);
 			idx = 1;
-			cState.setInt(idx++, cCheckLogin.m_nUserId); // follows_0000.user_id=?
-			if(!strCondBlock.isEmpty()) cState.setInt(idx++, cCheckLogin.m_nUserId); // blocks_0000.user_id=?
-			if(!strCondBlocked.isEmpty()) cState.setInt(idx++, cCheckLogin.m_nUserId); // blocks_0000.block_user_id=?
-			cState.setInt(idx++, cCheckLogin.m_nSafeFilter); // safe_filter<=?
-			cState.setInt(idx++, cCheckLogin.m_nUserId); // follow_tags_0000.user_id=?
-			if(!strCondSearch.isEmpty()) cState.setString(idx++, strSearchKeyword);
-			if(!strCondMute.isEmpty()) cState.setString(idx++, strMuteKeyword);
-			cState.setInt(idx++, m_nPage * SELECT_MAX_GALLERY);
-			cState.setInt(idx++, SELECT_MAX_GALLERY); // LIMIT ?
-			cResSet = cState.executeQuery();
-			while (cResSet.next()) {
-				CContent cContent = new CContent(cResSet);
-				cContent.m_cUser.m_strNickName	= Util.toString(cResSet.getString("nickname"));
-				cContent.m_cUser.m_strFileName	= Util.toString(cResSet.getString("user_file_name"));
-				if(cContent.m_cUser.m_strFileName.isEmpty()) cContent.m_cUser.m_strFileName="/img/default_user.jpg";
-				cContent.m_cUser.m_nReaction = cResSet.getInt("ng_reaction");
-				cContent.m_cUser.m_nFollowing = (cContent.m_nUserId == cCheckLogin.m_nUserId)?CUser.FOLLOW_HIDE:(cResSet.getInt("follow_user_id")>0)?CUser.FOLLOW_FOLLOWING:CUser.FOLLOW_NONE;
-				m_nEndId = cContent.m_nContentId;
-				m_vContentList.add(cContent);
+			statement.setInt(idx++, cCheckLogin.m_nUserId); // follows_0000.user_id=?
+			if(!strCondBlock.isEmpty()) statement.setInt(idx++, cCheckLogin.m_nUserId); // blocks_0000.user_id=?
+			if(!strCondBlocked.isEmpty()) statement.setInt(idx++, cCheckLogin.m_nUserId); // blocks_0000.block_user_id=?
+			statement.setInt(idx++, cCheckLogin.m_nSafeFilter); // safe_filter<=?
+			statement.setInt(idx++, cCheckLogin.m_nUserId); // follow_tags_0000.user_id=?
+			if(!strCondSearch.isEmpty()) statement.setString(idx++, strSearchKeyword);
+			if(!strCondMute.isEmpty()) statement.setString(idx++, strMuteKeyword);
+			resultSet = statement.executeQuery();
+			if (resultSet.next()) {
+				m_nContentsNum = resultSet.getInt(1);
 			}
-			cResSet.close();cResSet=null;
-			cState.close();cState=null;
+			resultSet.close();resultSet=null;
+			statement.close();statement=null;
+
+			strSql = "SELECT contents_0000.*, follows_0000.follow_user_id " + strSqlFromWhere
+					+ " ORDER BY content_id DESC OFFSET ? LIMIT ?";
+			statement = connection.prepareStatement(strSql);
+			idx = 1;
+			statement.setInt(idx++, cCheckLogin.m_nUserId); // follows_0000.user_id=?
+			if(!strCondBlock.isEmpty()) statement.setInt(idx++, cCheckLogin.m_nUserId); // blocks_0000.user_id=?
+			if(!strCondBlocked.isEmpty()) statement.setInt(idx++, cCheckLogin.m_nUserId); // blocks_0000.block_user_id=?
+			statement.setInt(idx++, cCheckLogin.m_nSafeFilter); // safe_filter<=?
+			statement.setInt(idx++, cCheckLogin.m_nUserId); // follow_tags_0000.user_id=?
+			if(!strCondSearch.isEmpty()) statement.setString(idx++, strSearchKeyword);
+			if(!strCondMute.isEmpty()) statement.setString(idx++, strMuteKeyword);
+			statement.setInt(idx++, m_nPage * SELECT_MAX_GALLERY);
+			statement.setInt(idx++, SELECT_MAX_GALLERY); // LIMIT ?
+			resultSet = statement.executeQuery();
+			while (resultSet.next()) {
+				CContent content = new CContent(resultSet);
+				CacheUsers0000.User user = users.getUser(content.m_nUserId);
+				content.m_cUser.m_strNickName	= Util.toString(user.m_strNickName);
+				content.m_cUser.m_strFileName	= Util.toString(user.m_strFileName);
+				content.m_cUser.m_nReaction		= user.m_nReaction;
+				content.m_cUser.m_nFollowing	= (content.m_nUserId == cCheckLogin.m_nUserId)?CUser.FOLLOW_HIDE:(resultSet.getInt("follow_user_id")>0)?CUser.FOLLOW_FOLLOWING:CUser.FOLLOW_NONE;
+				m_nEndId = content.m_nContentId;
+				m_vContentList.add(content);
+			}
+			resultSet.close();resultSet=null;
+			statement.close();statement=null;
 
 			bRtn = true;	// 以下エラーが有ってもOK.表示は行う
 
 			// Each Comment
-			GridUtil.getEachComment(cConn, m_vContentList);
+			GridUtil.getEachComment(connection, m_vContentList);
 
 			// Bookmark
 			if(cCheckLogin.m_bLogin) {
-				GridUtil.getEachBookmark(cConn, m_vContentList, cCheckLogin);
+				GridUtil.getEachBookmark(connection, m_vContentList, cCheckLogin);
 			}
 		} catch(Exception e) {
 			Log.d(strSql);
 			e.printStackTrace();
 		} finally {
-			try{if(cResSet!=null){cResSet.close();cResSet=null;}}catch(Exception e){;}
-			try{if(cState!=null){cState.close();cState=null;}}catch(Exception e){;}
-			try{if(cConn!=null){cConn.close();cConn=null;}}catch(Exception e){;}
+			try{if(resultSet!=null){resultSet.close();resultSet=null;}}catch(Exception e){;}
+			try{if(statement!=null){statement.close();statement=null;}}catch(Exception e){;}
+			try{if(connection!=null){connection.close();connection=null;}}catch(Exception e){;}
 		}
 		return bRtn;
 	}
-
 }
 
--- src/jp/pipa/poipiku/util/SqlUtil.java
+++ src/jp/pipa/poipiku/util/SqlUtil.java
@@ -6,52 +6,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class SqlUtil {
-	public static String getBlockUserSql(Connection connection, int nUserId) throws SQLException {
-		String strRet = "";
-		String sql = "SELECT block_user_id FROM blocks_0000 WHERE user_id=? LIMIT 1";
-		PreparedStatement statement = connection.prepareStatement(sql);
-		statement.setInt(1, nUserId);
-		ResultSet resultSet = statement.executeQuery();
-		if(resultSet.next()) {
-			strRet = " AND users_0000.user_id NOT IN(SELECT block_user_id FROM blocks_0000 WHERE user_id=?)";
-		}
-		resultSet.close();resultSet=null;
-		statement.close();statement=null;
-		return strRet;
-	}
-
-	public static String getBlockedUserSql(Connection connection, int nUserId) throws SQLException {
-		String strRet = "";
-		String sql = "SELECT user_id FROM blocks_0000 WHERE block_user_id=? LIMIT 1";
-		PreparedStatement statement = connection.prepareStatement(sql);
-		statement.setInt(1, nUserId);
-		ResultSet resultSet = statement.executeQuery();
-		if(resultSet.next()) {
-			strRet = " AND users_0000.user_id NOT IN(SELECT user_id FROM blocks_0000 WHERE block_user_id=?)";
-		}
-		resultSet.close();resultSet=null;
-		statement.close();statement=null;
-		return strRet;
-	}
-
-	public static String getSearhKeyWord(Connection connection, int nUserId) throws SQLException {
-		String sql = "SELECT tag_txt FROM follow_tags_0000 WHERE user_id=? AND type_id=1 LIMIT 100";
-		PreparedStatement statement = connection.prepareStatement(sql);
-		statement.setInt(1, nUserId);
-		ResultSet resultSet = statement.executeQuery();
-		StringBuilder sbKeyWord = new StringBuilder();
-		if(resultSet.next()) {
-			sbKeyWord.append(Util.toString(resultSet.getString(1)).trim());
-			while (resultSet.next()) {
-				sbKeyWord.append(" OR ");
-				sbKeyWord.append(Util.toString(resultSet.getString(1)).trim());
-			}
-		}
-		resultSet.close();resultSet=null;
-		statement.close();statement=null;
-		return sbKeyWord.toString().trim();
-	}
-
 	public static String getMuteKeyWord(Connection connection, int nUserId) throws SQLException {
 		String strRet = "";
 		String sql = "SELECT mute_keyword_list FROM users_0000 WHERE user_id=?";
@@ -64,13 +18,5 @@ public class SqlUtil {
 		resultSet.close();resultSet=null;
 		statement.close();statement=null;
 		return strRet.trim();
-	}
-
-	public static String getMuteKeyWordNega(Connection connection, int nUserId) throws SQLException {
-		String strRet = getMuteKeyWord(connection, nUserId);
-		if(!strRet.isEmpty()) {
-			strRet = " -" + strRet.replace(" OR ", " -");
-		}
-		return strRet;
 	}
 }
