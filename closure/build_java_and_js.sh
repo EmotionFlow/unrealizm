bebd472420050cc8c2e1639d17aed4e3691266d4
nino <github@nerune.co>
2021-03-01 14:21:05 +0900

    PC固有の設定を.envファイルへ移動

--- closure/build_java_and_js.sh
+++ closure/build_java_and_js.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
+set -e
 
+ENV_FILE=.env
+if [ ! -f ${ENV_FILE} ]; then
+ echo ${ENV_FILE} is not found.
+ exit
+fi
+
+# shellcheck disable=SC1090
+source ${ENV_FILE}
+
+JAVAC=${JAVA_HOME}/bin/javac
 SOURCE_PATH="../src"
 DEST_PATH="../build"
 DEPLOY_PATH="../WebContent/WEB-INF/classes"
 CLASSES_WEB_INF="../WebContent/WEB-INF/lib/*"
-CLASSES_TOMCAT="/opt/java/tomcat/apache-tomcat-7.0.107/lib/*"
 JAVA_FILE_ROOT="../src/jp/pipa/poipiku"
 JAVA_FILE_DIRS=("/controller" "/servlet" "/util" "/settlement/epsilon" "/settlement")
-
-export JAVA_HOME="/Library/Java/JavaVirtualMachines/zulu-8.jdk"
 
 java -version
 
@@ -16,11 +24,11 @@ rm -rf $DEST_PATH
 mkdir $DEST_PATH
 
 echo /
-javac -Xlint:unchecked -sourcepath $SOURCE_PATH -d $DEST_PATH -cp $CLASSES_WEB_INF:$CLASSES_TOMCAT:$DEST_PATH $JAVA_FILE_ROOT/*.java
+${JAVAC} -Xlint:unchecked -sourcepath $SOURCE_PATH -d $DEST_PATH -cp $CLASSES_WEB_INF:$CLASSES_TOMCAT:$DEST_PATH $JAVA_FILE_ROOT/*.java
 
 for dir in "${JAVA_FILE_DIRS[@]}"; do
 		echo $dir
-		javac -Xlint:unchecked -sourcepath $SOURCE_PATH -d $DEST_PATH -cp $CLASSES_WEB_INF:$CLASSES_TOMCAT:$DEST_PATH $JAVA_FILE_ROOT$dir/*.java
+		${JAVAC} -Xlint:unchecked -sourcepath $SOURCE_PATH -d $DEST_PATH -cp $CLASSES_WEB_INF:$CLASSES_TOMCAT:$DEST_PATH $JAVA_FILE_ROOT$dir/*.java
 done
 
 echo build ok
