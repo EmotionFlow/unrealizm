ba45ed4013355c83af51c3ff76d2538fc8da8a7f
nino <github@nerune.co>
2021-03-04 11:43:42 +0900

    update message

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -12,12 +12,13 @@ source ${ENV_FILE}
 echo "本番サーバ上のリポジトリでgit pullを実行します"
 ssh ${HOST_ALIAS} 'bash -c "cd stg && ./pull.sh && cd poipiku && git branch && git log -1"'
 
-read -p "こちらのcommitを本番サーバにデプロイします。よろしいですか？(y/N): " yn
+read -p "こちらがHEADのcommitを本番サーバにデプロイします。よろしいですか？(y/N): " yn
 case "$yn" in [yY]*) ;; *) echo "abort." ; exit ;; esac
 
 ssh ${HOST_ALIAS} 'bash -c "cd stg && ./deploy_prod.sh -f"'
 
 read -p "デプロイしました。tomcatを再起動しますか？(y/N): " yn
-case "$yn" in [yY]*) ;; *) echo "abort." ; exit ;; esac
+case "$yn" in [yY]*) ;; *) echo "tomcatを再起動せず終了しました。" ; exit ;; esac
 
 ssh ${HOST_ALIAS} 'sudo systemctl restart tomcat.service'
+echo "本番サーバのtomcatを再起動しました"
