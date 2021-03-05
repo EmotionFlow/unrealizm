fdee444d7e916a975648dc99dabf0ddf98891bb8
nino <github@nerune.co>
2021-03-05 15:48:49 +0900

    ビルドコマンド追加

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -9,10 +9,11 @@ fi
 # shellcheck disable=SC1090
 source ${ENV_FILE}
 
-echo "本番サーバ上のリポジトリでgit pullを実行します"
-ssh ${HOST_ALIAS} 'bash -c "cd stg && ./pull.sh && cd poipiku && git branch && git log -1"'
+echo "本番サーバでpull,buildを実行します"
+ssh ${HOST_ALIAS} 'bash -c "cd stg && ./pull.sh && ./build_java_and_js.sh && cd poipiku && git branch && git log -1"'
 
-read -p "こちらがHEADのcommitを本番サーバにデプロイします。よろしいですか？(y/N): " yn
+echo "."
+read -p "このcommitを本番サーバにデプロイします。よろしいですか？(y/N): " yn
 case "$yn" in [yY]*) ;; *) echo "abort." ; exit ;; esac
 
 ssh ${HOST_ALIAS} 'bash -c "cd stg && ./deploy_prod.sh -f"'
