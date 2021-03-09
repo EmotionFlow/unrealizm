066c72b2ddbb99552e4d9fca5706eb7fade07c6d
nino <github@nerune.co>
2021-03-09 20:58:44 +0900

    make release時にreleaseタグを打つ処理を追加

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -18,4 +18,7 @@ case "$yn" in [yY]*) ;; *) echo "abort." ; exit ;; esac
 
 ssh ${HOST_ALIAS} 'bash -c "cd stg && ./deploy_prod.sh -f"'
 
+git tag -a release-$(date +%Y-%m-%d-%H-%M) -m "Release at $(date)"
+git push --tags
+
 echo "デプロイしました"
