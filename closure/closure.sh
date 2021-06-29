76032513a5b36c192855a2da2e8d0e948ab6d5e4
nino <github@nerune.co>
2021-06-29 13:41:52 +0900

    img-cdn.poipiku.com (cloudfront) 廃止、img.poipiku.com (cloudflare) に統一。

--- closure/closure.sh
+++ closure/closure.sh
@@ -3,13 +3,13 @@
 /bin/rm -f ../WebContent/js/upload-*.js
 /bin/rm -f ../WebContent/js/update-*.js
 
-/bin/cp ./js/common-50.js ../WebContent/js/
+/bin/cp ./js/common-51.js ../WebContent/js/
 #/bin/cp ./js/commonPc-03.js ../WebContent/js/
 /bin/cp ./js/upload-39.js ../WebContent/js/
 /bin/cp ./js/update-13.js ../WebContent/js/
 
 CLOSURE_COMPILER_JAR="./closure-compiler-v20201006.jar"
-java -jar ${CLOSURE_COMPILER_JAR} --js ./js/common-50.js --js_output_file ../WebContent/js/common-50.js
+java -jar ${CLOSURE_COMPILER_JAR} --js ./js/common-51.js --js_output_file ../WebContent/js/common-51.js
 #java -jar ${CLOSURE_COMPILER_JAR} --js ./js/commonPc-03.js --js_output_file ../WebContent/js/commonPc-03.js
 java -jar ${CLOSURE_COMPILER_JAR} --js ./js/upload-39.js --js_output_file ../WebContent/js/upload-39.js
 java -jar ${CLOSURE_COMPILER_JAR} --js ./js/update-13.js --js_output_file ../WebContent/js/update-13.js
