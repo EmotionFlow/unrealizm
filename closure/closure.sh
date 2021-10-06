0be028c6a2f4fd7ea3ea7f8a9ae0b4f72aa7b119
nino <github@nerune.co>
2021-10-06 15:07:13 +0900

    jsファイル名更新

--- closure/closure.sh
+++ closure/closure.sh
@@ -3,13 +3,13 @@
 /bin/rm -f ../WebContent/js/upload-*.js
 /bin/rm -f ../WebContent/js/update-*.js
 
-/bin/cp ./js/common-63.js ../WebContent/js/
+/bin/cp ./js/common-64.js ../WebContent/js/
 #/bin/cp ./js/commonPc-03.js ../WebContent/js/
 /bin/cp ./js/upload-41.js ../WebContent/js/
 /bin/cp ./js/update-15.js ../WebContent/js/
 
 CLOSURE_COMPILER_JAR="./closure-compiler-v20201006.jar"
-java -jar ${CLOSURE_COMPILER_JAR} --js ./js/common-63.js --js_output_file ../WebContent/js/common-63.js
+java -jar ${CLOSURE_COMPILER_JAR} --js ./js/common-64.js --js_output_file ../WebContent/js/common-64.js
 #java -jar ${CLOSURE_COMPILER_JAR} --js ./js/commonPc-03.js --js_output_file ../WebContent/js/commonPc-03.js
 java -jar ${CLOSURE_COMPILER_JAR} --js ./js/upload-41.js --js_output_file ../WebContent/js/upload-41.js
 java -jar ${CLOSURE_COMPILER_JAR} --js ./js/update-15.js --js_output_file ../WebContent/js/update-15.js
