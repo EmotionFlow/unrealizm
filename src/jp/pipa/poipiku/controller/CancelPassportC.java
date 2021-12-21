002bb33249995686cfad146b78700f42a3264263
nino <github@nerune.co>
2021-12-21 11:50:36 +0900

    ログ出力追加

--- src/jp/pipa/poipiku/controller/CancelPassportC.java
+++ src/jp/pipa/poipiku/controller/CancelPassportC.java
@@ -3,14 +3,25 @@ package jp.pipa.poipiku.controller;
 import jp.pipa.poipiku.CheckLogin;
 import jp.pipa.poipiku.Passport;
 import jp.pipa.poipiku.PassportSubscription;
+import jp.pipa.poipiku.util.Log;
 
 public final class CancelPassportC {
 	public int m_nErrCode = PassportSubscription.ErrorKind.Unknown.getCode();
 
 	public boolean getResults(CheckLogin checkLogin, CancelPassportCParam cParam) {
-		if(cParam.m_nUserId<0) return false;
-		if(!checkLogin.m_bLogin) return false;
-		if(cParam.m_nUserId != checkLogin.m_nUserId) return false;
+		Log.d("enter");
+		if(cParam.m_nUserId<0){
+			Log.d("cParam.m_nUserId<0");
+			return false;
+		}
+		if(!checkLogin.m_bLogin){
+			Log.d("!checkLogin.m_bLogin");
+			return false;
+		}
+		if(cParam.m_nUserId != checkLogin.m_nUserId){
+			Log.d("cParam.m_nUserId != checkLogin.m_nUserId");
+			return false;
+		}
 
 		PassportSubscription subscription = new PassportSubscription(checkLogin);
 		boolean result = subscription.cancel();
