1ee2468a2e1e301cbf46f300865fdf16bdba97cb
nino <github@nerune.co>
2022-01-07 17:16:34 +0900

    リアクション通知未読ユーザーへのメール配信

---
+++ dev-tool/translate/translate_message_template.py
@@ -0,0 +1,54 @@
+# 既存のpropertiesファイルを走査して、新たに別の言語のpropertiesファイルを生成するスクリプト
+
+import requests
+import json
+import sys
+import os
+import glob
+import translate
+
+# 元の言語
+SOURCE_LOCALE = 'en'
+
+# ターゲットとする言語
+#TARGET_LOCALES = ['ko', 'zh_CN', 'zh_TW', 'th', 'ru', 'vi']
+#TARGET_LOCALES = ['ko']
+
+TEMPLATE_ROOT = '../../WebContent/WEB-INF/message_templates/'
+TEMPLATE_PATH = sys.argv[1]
+PROP_EXT = '.properties'
+
+source_path = f"{TEMPLATE_ROOT}{SOURCE_LOCALE}/{TEMPLATE_PATH}/"
+
+for target_locale in translate.TARGET_LOCALES:
+    dest_path = f"{TEMPLATE_ROOT}{target_locale}/{TEMPLATE_PATH}/"
+    if os.path.exists(dest_path):
+        print(f"{dest_path} is exist")
+        continue
+    os.makedirs(dest_path)
+    vm_files = glob.glob(source_path + "*.vm")
+    for vm_file in vm_files:
+        print(vm_file)
+        f = open(vm_file, 'r')
+        lines = f.readlines()
+        f.close()
+        trans_lines = []
+        for line in lines:
+            line = line.replace(f'#parse("{SOURCE_LOCALE}', f'#parse("{target_locale}')
+            if line[0] == "#" or line[0] == '':
+                trans_lines.append(line)
+            else:
+                try:
+                    word = line.encode('ascii').decode('unicode-escape')
+                    print(word)
+                    response = requests.post(f"{translate.SCRIPT_URI}", data={'text': word, 'source': SOURCE_LOCALE, 'target': translate.GOOGLE_TRANSLATE_LOCALES[target_locale]})
+                    resp = json.loads(response.text)
+                    trans_word = resp['text']
+                    trans_word = trans_word.replace("$ ", "$")
+                    trans_lines.append(trans_word)
+                except UnicodeDecodeError:
+                    pass
+                    # target_file.write(line)
+        f = open(f"{dest_path}/{os.path.basename(vm_file)}", 'w')
+        f.write("".join(trans_lines))
+        f.close()
---
+++ src/jp/pipa/poipiku/notify/ReactionsHasNotCheckedNotifier.java
@@ -0,0 +1,132 @@
+package jp.pipa.poipiku.notify;
+
+import jp.pipa.poipiku.Common;
+import jp.pipa.poipiku.util.Log;
+import org.apache.velocity.Template;
+import org.apache.velocity.VelocityContext;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public final class ReactionsHasNotCheckedNotifier extends Notifier {
+	private static final int MAX_SEND_USERS = 500;
+
+	public ReactionsHasNotCheckedNotifier() {
+		CATEGORY = "has_not_checked";
+	}
+
+	public boolean notifyToReactionHasNotCheckedUser(DataSource dataSource) {
+		Connection connection = null;
+		PreparedStatement statement = null;
+		ResultSet resultSet = null;
+		String sql = "";
+
+		boolean result = false;
+		try {
+			connection = dataSource.getConnection();
+			// １ヶ月以上アクセスしていないユーザーのうち、４週間以上DMしていないユーザー
+			// かつ、DM許可しているユーザー。今までDMしていない人を優先。
+			sql = "SELECT i.user_id, u.email, u.nickname, u.lang_id" +
+					" FROM info_lists i" +
+					"         INNER JOIN users_0000 u ON i.user_id = u.user_id" +
+					" WHERE i.info_type = 1" +
+					"  AND i.had_read = FALSE" +
+//					"  AND i.info_date < (now() - INTERVAL '14 days')" +
+					"  AND i.info_date < (timestamp '2022-01-06 00:00:00' - INTERVAL '14 days')" +
+					"  AND u.email LIKE '%@%'" +
+					"  AND u.hash_password NOT LIKE '%BAN'" +
+					"  AND u.email NOT LIKE '%BAN'" +
+					"  AND i.sent_unread_mail_at IS NULL" +
+					"  AND send_email_mode = 1" +
+					" GROUP BY i.user_id, u.email, u.nickname, u.lang_id" +
+					" LIMIT ?";
+			statement = connection.prepareStatement(sql);
+			statement.setInt(1, MAX_SEND_USERS);
+			resultSet = statement.executeQuery();
+			List<User> deliveryTargets = new ArrayList<>();
+			while (resultSet.next()) {
+				User u = new User();
+				u.id = resultSet.getInt("user_id");
+				u.email = resultSet.getString("email");
+				u.nickname = resultSet.getString("nickname");
+				u.langId = resultSet.getInt("lang_id");
+				u.setLangLabel();
+				deliveryTargets.add(u);
+			}
+			resultSet.close();
+			statement.close();
+
+			if (deliveryTargets.isEmpty()) {
+				Log.d("配信対象なし");
+				return true;
+			}
+
+			sql = "WITH a AS (" +
+					"    SELECT content_id, SUM(badge_num) badge_sum" +
+					"    FROM info_lists i" +
+					"    WHERE i.info_type = 1" +
+					"      AND i.had_read = FALSE" +
+					"      AND i.user_id = ?" +
+					"    GROUP BY content_id" +
+					" )" +
+					" SELECT COUNT(*) contents_total, SUM(badge_sum) badge_total" +
+					" FROM a;";
+			statement = connection.prepareStatement(sql);
+			for (User targetUser : deliveryTargets) {
+				// 未読通知のコンテンツ数をリアクション数の合計を求める
+				statement.setInt(1, targetUser.id);
+				resultSet = statement.executeQuery();
+
+				if (resultSet.next()) {
+					int contentsTotal = resultSet.getInt(1);
+					int reactionsTotal = resultSet.getInt(2);
+					resultSet.close();
+
+					// メール本文生成
+					String mailSubject = getSubject("reactions", targetUser.langLabel);
+
+					VelocityContext context = new VelocityContext();
+					context.put("to_name", targetUser.nickname);
+					context.put("contents_total", contentsTotal);
+					context.put("reactions_total", reactionsTotal);
+
+					Template template = getBodyTemplate("reactions", targetUser.langLabel);
+					String mailBody = merge(template, context);
+
+					// 配信
+					notifyByEmail(targetUser, mailSubject, mailBody);
+
+					// 配信日時設定
+//				sql = "UPDATE info_lists SET sent_unread_mail_at=now() WHERE user_id=?";
+//				statement = connection.prepareStatement(sql);
+//				statement.setInt(1, targetUser.id);
+//				statement.executeUpdate();
+//				statement.close();
+
+					// Amazon SES Maximum send rate is
+					// 14 emails per second
+					Thread.sleep(1000);
+				} else {
+					resultSet.close();
+				}
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			Log.d(sql);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		} finally {
+			if(resultSet!=null){try{resultSet.close();}catch(SQLException ignored){}};
+			if(statement!=null){try{statement.close();}catch(SQLException ignored){}};
+			if(connection!=null){try{connection.close();}catch(SQLException ignored){}};
+		}
+
+		return result;
+	}
+}
