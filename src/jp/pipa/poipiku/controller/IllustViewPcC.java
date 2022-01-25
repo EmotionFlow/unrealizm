ca7c3f1797ae70d1db8b200f852ab3d125d60cef
nino <github@nerune.co>
2022-01-25 14:34:39 +0900

    負荷軽減のために止めていた処理を復活

--- src/jp/pipa/poipiku/controller/IllustViewPcC.java
+++ src/jp/pipa/poipiku/controller/IllustViewPcC.java
@@ -268,17 +268,15 @@ public final class IllustViewPcC {
 					m_vContentList = RelatedContents.getUserContentList(ownerUserId, selectMaxGallery, checkLogin, connection);
 				}
 
-//				final int h = LocalDateTime.now().getHour();
-//				if (h != 23 && h != 0 && h != 1) {
-				if (false) {
+				final int h = LocalDateTime.now().getHour();
+				if (h != 23 && h != 0 && h != 1) {
 					// Related Contents
 					if (selectMaxRelatedGallery > 0) {
 						m_vRelatedContentList = RelatedContents.getGenreContentList(m_cContent.m_nContentId, selectMaxRelatedGallery, checkLogin, connection);
 					}
 				}
 
-				if (false) {
-//				if (h != 21 && h != 22 && h != 23 && h != 0 && h != 1){
+				if (h != 21 && h != 22 && h != 23 && h != 0 && h != 1){
 					// Recommended Contents
 					if(selectMaxRecommendedGallery >0) {
 						m_vRecommendedList = RecommendedContents.getContents(m_cContent.m_nUserId, m_cContent.m_nContentId, selectMaxRecommendedGallery, checkLogin, connection);
