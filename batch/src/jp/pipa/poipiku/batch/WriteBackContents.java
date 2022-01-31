4609442493b25cf7056880833a6100d191b99b07
nino <github@nerune.co>
2022-01-31 10:19:21 +0900

    SSD->HDDバッチ調整

--- batch/src/jp/pipa/poipiku/batch/WriteBackContents.java
+++ batch/src/jp/pipa/poipiku/batch/WriteBackContents.java
@@ -19,10 +19,10 @@ import java.util.List;
 
 public class WriteBackContents extends Batch {
 	// SSD上にファイルを保持する時間
-	static final int HOLD_IN_CACHE_HOURS = 36 + 48;
+	static final int HOLD_IN_CACHE_HOURS = 36 + 24;
 
 	// 一度のバッチ実行でselectするファイルの最大数
-	static final int SELECT_LIMIT = 30;
+	static final int SELECT_LIMIT = 35;
 
 	// HDDへの移動後も、DBにレコードを保持しておく時間
 	static final int HOLD_AFTER_RECORD_MOVED_HOURS = 180;
@@ -70,7 +70,6 @@ public class WriteBackContents extends Batch {
 		}
 		Log.d("moveTargets.size(): " + moveTargets.size());
 
-		int cnt = 1;
 		for (WriteBackFile writeBackFile: moveTargets) {
 			writeBackFile.updateStatus(WriteBackFile.Status.Moving);
 
