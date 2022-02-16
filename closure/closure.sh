94974946abf1e48233e45ea47347ff37c675afc8
nino <github@nerune.co>
2022-02-16 10:30:45 +0900

    update filename

--- closure/closure.sh
+++ closure/closure.sh
@@ -5,13 +5,13 @@
 
 /bin/cp ./js/common-99.js ../WebContent/js/
 #/bin/cp ./js/commonPc-04.js ../WebContent/js/
-/bin/cp ./js/upload-47.js ../WebContent/js/
+/bin/cp ./js/upload-48.js ../WebContent/js/
 /bin/cp ./js/update-20.js ../WebContent/js/
 
 CLOSURE_COMPILER_JAR="./closure-compiler-v20201006.jar"
 java -jar ${CLOSURE_COMPILER_JAR} --js ./js/common-99.js --js_output_file ../WebContent/js/common-99.js
 #java -jar ${CLOSURE_COMPILER_JAR} --js ./js/commonPc-04.js --js_output_file ../WebContent/js/commonPc-04.js
-java -jar ${CLOSURE_COMPILER_JAR} --js ./js/upload-47.js --js_output_file ../WebContent/js/upload-47.js
+java -jar ${CLOSURE_COMPILER_JAR} --js ./js/upload-48.js --js_output_file ../WebContent/js/upload-48.js
 java -jar ${CLOSURE_COMPILER_JAR} --js ./js/update-20.js --js_output_file ../WebContent/js/update-20.js
 
 ##for file in $( ls ./js | grep .js$ ); do
