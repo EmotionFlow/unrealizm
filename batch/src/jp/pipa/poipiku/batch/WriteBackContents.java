4566ee9e09cd733b5a40d49d3e62b8622dd6fc1d
nino <github@nerune.co>
2022-02-28 12:20:01 +0900

    負荷調整

--- batch/src/jp/pipa/poipiku/batch/WriteBackContents.java
+++ batch/src/jp/pipa/poipiku/batch/WriteBackContents.java
@@ -20,10 +20,10 @@ import java.util.List;
 
 public class WriteBackContents extends Batch {
 	// SSD上にファイルを保持する時間
-	static final int HOLD_IN_CACHE_HOURS = 36 + 24;
+	static final int HOLD_IN_CACHE_HOURS = 36;
 
 	// 一度のバッチ実行でselectするファイルの最大数
-	static final int SELECT_LIMIT = 35;
+	static final int SELECT_LIMIT = 40;
 
 	// HDDへの移動後も、DBにレコードを保持しておく時間
 	static final int HOLD_AFTER_RECORD_MOVED_HOURS = 180;
@@ -57,7 +57,7 @@ public class WriteBackContents extends Batch {
 		Log.d("WriteBackContents batch start");
 
 		final int h = LocalDateTime.now().getHour();
-		if (h == 21 || h == 22 ||h == 23 || h == 0 || h == 1){
+		if (h == 22 ||h == 23 || h == 0 || h == 1){
 			Log.d("処理時間外");
 			Log.d("WriteBackContents batch end");
 			return;
