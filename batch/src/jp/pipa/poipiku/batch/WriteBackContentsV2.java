c1973a8cddf1ba821736a53f96112849f03d28f0
nino <github@nerune.co>
2022-05-18 09:54:05 +0900

    img01への書き込み調整

--- batch/src/jp/pipa/poipiku/batch/WriteBackContentsV2.java
+++ batch/src/jp/pipa/poipiku/batch/WriteBackContentsV2.java
@@ -26,7 +26,7 @@ public class WriteBackContentsV2 extends Batch {
 
 	// 一度のバッチ実行でselectするファイルの最大数
 	private static int getSelectLimit() {
-		int selectLimit = 31;
+		int selectLimit = 30;
 		Calendar calendar = Calendar.getInstance();
 		final int dayOfWeek = calendar.get(Calendar.DAY_OF_WEEK);
 		final int hour = LocalTime.now().getHour();
@@ -36,7 +36,6 @@ public class WriteBackContentsV2 extends Batch {
 			selectLimit = 40;
 		}
 
-		selectLimit = 10;
 		return selectLimit;
 	}
 
