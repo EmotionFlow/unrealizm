2868711279749daced6a0e9d78e0be29cdd63604
nino <github@nerune.co>
2022-08-02 13:18:27 +0900

    翻訳apiのgoogleアカウント変更

--- dev-tool/translate/translate.py
+++ dev-tool/translate/translate.py
@@ -1,4 +1,4 @@
-SCRIPT_URI = 'https://script.google.com/macros/s/AKfycbwR-f-m4rQXtV_W3Hi5fCpf8ZnyecKA387CWGWg1qHvF5jAOHzU4VayDq_rKTHgJ6aO/exec'
+SCRIPT_URI = 'https://script.google.com/macros/s/AKfycbwhy5pj1VlSdhY9F75B551S9ecuK1Eq71ScSNMv-9Mst2-D3SznTLymWdrIsAlRKhfWQQ/exec'
 
 # 対象言語。rs_??.propertiesの??部分
 TARGET_LOCALES = ['ko', 'zh_CN', 'zh_TW', 'th', 'ru', 'vi', 'es']
