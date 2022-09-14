25f8b3c8bb392be436a17c6398ec7b924065887d
nino <github@nerune.co>
2022-09-14 14:19:46 +0900

    "開発環境のjdbc接続先について、レプリケーション環境を追加しないでも良いように変更"をロールバック

--- src/jp/pipa/poipiku/util/DatabaseUtil.java
+++ src/jp/pipa/poipiku/util/DatabaseUtil.java
@@ -13,12 +13,7 @@ public final class DatabaseUtil {
 		try {
 			Class.forName("org.postgresql.Driver");
 			dataSource = (DataSource)new InitialContext().lookup(Common.DB_POSTGRESQL);
-			if (Common.isDevEnv()) {
-				Log.d("dev mode, replicaDataSource = dataSource");
-				replicaDataSource = dataSource;
-			} else {
-				replicaDataSource = (DataSource)new InitialContext().lookup(Common.DB_POSTGRESQL_REPLICA);
-			}
+			replicaDataSource = (DataSource)new InitialContext().lookup(Common.DB_POSTGRESQL_REPLICA);
 		} catch (ClassNotFoundException | NamingException e) {
 			e.printStackTrace();
 			dataSource = null;
