700ca32ddfafb80a347fd4a8f3b01740f7161660
nino <github@nerune.co>
2022-10-19 21:17:58 +0900

    デプロイスクリプト更新

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -9,16 +9,16 @@ fi
 # shellcheck disable=SC1090
 source ${ENV_FILE}
 
-echo "本番サーバでpull,buildを実行します"
-ssh ${HOST_ALIAS} 'bash -c "cd stg && ./poipiku_pull.sh && ./poipiku_build.sh && cd poipiku && git branch && git log -1"'
+echo "本番サーバでai-poipiku環境のpull,buildを実行します"
+ssh ${HOST_ALIAS} 'bash -c "cd stg && ./ai-poipiku_pull.sh && ./ai-poipiku_build.sh && cd poipiku && git branch && git log -1"'
 
 echo "."
-read -p "このcommitを本番サーバにデプロイします。よろしいですか？(y/N): " yn
+read -p "このcommitをai-poipiku本番サーバにデプロイします。よろしいですか？(y/N): " yn
 case "$yn" in [yY]*) ;; *) echo "abort." ; exit ;; esac
 
-ssh ${HOST_ALIAS} 'bash -c "cd stg && sudo ./poipiku_deploy_prod.sh -f"'
+ssh ${HOST_ALIAS} 'bash -c "cd stg && sudo ./ai-poipiku_deploy_prod.sh -f"'
 
-git tag -a release-$(date +%Y-%m-%d-%H-%M) -m "Release at $(date)"
+git tag -a release-ai-$(date +%Y-%m-%d-%H-%M) -m "Release at $(date)"
 git push --tags
 
-echo "デプロイしました"
+echo "ai-poipikuをデプロイしました"
